c7dd63e2a9174380729bf4c33e9d8e9e2ff971e9
Misuri M Wijewardhana <misuri.malindika@gmail.com>
2023-07-27 10:49:34 +0530

    Add files via upload

---
+++ Lab1.2023.04.24/Q1.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.q1;
+
+/**
+ *
+ * @author Misuri
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Lab1.2023.04.24/Q3.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.q3;
+
+/**
+ *
+ * @author Misuri
+ */
+
+public class Q3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("executing loop " + i);
+        }
+    }
+}
+/*
+public class whileloop{
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("executing loop " + i);
+            i++;
+        }
+    }
+}
+*/
